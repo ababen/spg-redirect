64ff0a0bcfd3dc032e03afe6395705aa93217a43
Alexander Babenchuk <alexbaben@gmail.com>
2019-05-28 17:48:09 -0400

    Worked on this

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, render_template, redirect
-from math import floor
+from flask import Flask, request, render_template, redirect, g
 from sqlite3 import OperationalError
-import string
 import sqlite3
+
+from math import floor
+import string
 try:
     from urllib.parse import urlparse  # Python 3
     str_encode = str.encode
@@ -17,16 +18,18 @@ except ImportError:
     from string import uppercase as ascii_uppercase
 import base64
 
+
 # Assuming urls.db is in your app root folder
 app = Flask(__name__)
 host = 'http://localhost:5000/'
 
-
+'''
 def table_check():
     create_table = """
         CREATE TABLE WEB_URL(
         ID INT PRIMARY KEY AUTOINCREMENT,
-        URL TEXT NOT NULL
+        URL TEXT NOT NULL,
+        TIMES INT NOT NULL
         );
         """
     with sqlite3.connect('urls.db') as conn:
@@ -35,8 +38,16 @@ def table_check():
             cursor.execute(create_table)
         except OperationalError:
             pass
+'''
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('urls.db')
+    return db
 
 
+'''
 def toBase62(num, b=62):
     if b <= 0 or b > 62:
         return 0
@@ -50,7 +61,6 @@ def toBase62(num, b=62):
         res = base[int(r)] + res
     return res
 
-
 def toBase10(num, b=62):
     base = string.digits + ascii_lowercase + ascii_uppercase
     limit = len(num)
@@ -58,44 +68,74 @@ def toBase10(num, b=62):
     for i in range(limit):
         res = b * res + base.find(num[i])
     return res
-
-
+'''
+'''
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         original_url = str_encode(request.form.get('url'))
+        times = request.form.get('times')
         if urlparse(original_url).scheme == '':
-            url = 'http://' + original_url
+            url = 'https://' + original_url
         else:
             url = original_url
         with sqlite3.connect('urls.db') as conn:
             cursor = conn.cursor()
             res = cursor.execute(
-                'INSERT INTO WEB_URL (URL) VALUES (?)',
-                [base64.urlsafe_b64encode(url)]
+                'INSERT INTO WEB_URL (URL, TIMES) VALUES (?, ?)',
+                [base64.urlsafe_b64encode(url), times]
             )
             encoded_string = toBase62(res.lastrowid)
         return render_template('home.html', short_url=host + encoded_string)
     return render_template('home.html')
+'''
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchone()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.route('/')
 
 @app.route('/<short_url>')
 def redirect_short_url(short_url):
-    decoded = toBase10(short_url)
+    # decoded = toBase10(short_url)
     url = host  # fallback if no URL is found
+    result = query_db('SELECT * FROM RISP WHERE FILENO=?', [short_url])
+    print(result)
+    if result[2] < 1:
+        url = 'https://www.google.com'
+        with sqlite3.connect('urls.db') as conn:
+                    cursor = conn.cursor()
+                    sql = "UPDATE RISP SET USED = %s WHERE FILENO = %s"
+                    val = (result[2]+1, result[1])
+                    cursor.execute(sql, val)
+                    conn.execute()
+                    print(cursor.rowcount, "record(s) affected")
+    else:
+        print('This URL has already been used.')
+        url = 'https://www.aol..com'
+    return redirect(url)
+
+    '''    
     with sqlite3.connect('urls.db') as conn:
         cursor = conn.cursor()
-        res = cursor.execute('SELECT URL FROM WEB_URL WHERE ID=?', [decoded])
+        res = cursor.execute('SELECT * FROM RISP WHERE FILENO=?', [short_url])
+        short = res.fetchone()
+
         try:
-            short = res.fetchone()
+            # short = res.fetchone()
             if short is not None:
-                url = base64.urlsafe_b64decode(short[0])
+                url = 'https://www.google.com'
         except Exception as e:
             print(e)
-    return redirect(url)
+        return redirect(url)
+    '''
+    
 
 
 if __name__ == '__main__':
     # This code checks whether database table is created or not
-    table_check()
+    # table_check()
     app.run(debug=True)
