87a578c44d1906b9231f09aa60587b6dee1f3d42
Alexander Babenchuk <alexbaben@gmail.com>
2019-05-28 20:46:01 -0400

    Created app.py

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, render_template, redirect, g
+from sqlite3 import OperationalError
+import sqlite3
+
+from math import floor
+import string
+try:
+    from urllib.parse import urlparse  # Python 3
+    str_encode = str.encode
+except ImportError:
+    from urlparse import urlparse  # Python 2
+    str_encode = str
+try:
+    from string import ascii_lowercase
+    from string import ascii_uppercase
+except ImportError:
+    from string import lowercase as ascii_lowercase
+    from string import uppercase as ascii_uppercase
+import base64
+
+
+# Assuming urls.db is in your app root folder
+app = Flask(__name__)
+host = 'https://spg-redirect.herokuapp.com/'
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchone()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        original_url = request.form.get('url')
+        if urlparse(original_url).scheme == '':
+            url = 'https://' + original_url
+        else:
+            url = original_url
+        with sqlite3.connect('urls.db') as conn:
+            cursor = conn.cursor()
+            res = cursor.execute(
+                'INSERT INTO WEB_URL (URL) VALUES (?, ?)', [url])
+        return render_template('home.html', short_url=host + url)
+    return render_template('home.html')
+
+
+@app.route('/<short_url>')
+def redirect_short_url(short_url):
+    # decoded = toBase10(short_url)
+    url = host  # fallback if no URL is found
+    try:
+        result = query_db('SELECT * FROM RISP WHERE FILENO=?', [short_url])
+        if result is not None and result[2] < 1:
+            url = 'https://www.spglawfirm.com/risperdal-message'
+            with sqlite3.connect('urls.db') as conn:
+                        value1 = int(result[2]) + 1
+                        value2 = result[1]
+                        cursor = conn.cursor()
+                        cursor.execute('UPDATE RISP SET USED = ? WHERE FILENO = ?', (value1, value2))
+        elif result is None:
+            message = 'We are not able to locate your case'
+            url = 'error.html'
+            return redirect(url, Response=message)
+        else:
+            print('This URL has already been used.')
+            url = 'https://www.spglawfirm.com/risperdal-message-thank-you/'
+        return redirect(url)
+    except Exception as e:
+        print(e)
+    return redirect(url)
+
+
+if __name__ == '__main__':
+    # This code checks whether database table is created or not
+    # table_check()
+    # app.run(debug=True)
+    app.run(host=host)
