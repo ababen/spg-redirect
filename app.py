b178b472c7a91ac9187eb227e0d550b7a64052e2
Alexander Babenchuk <alexbaben@gmail.com>
2019-06-05 16:31:30 -0400

    Changing code to switch to PSQL

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask, request, render_template, redirect, g
-from sqlite3 import OperationalError
-import sqlite3
+import os
 
-# Assuming urls.db is in your app root folder
+from flask import Flask, request, render_template, redirect, g
+# from sqlite3 import OperationalError
+# import sqlite3
+# from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
+# Define database location and name
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database = "sqlite:///{}".format(os.path.join(project_dir, "urls.db"))
+
 app = Flask(__name__)
-DATABASE = 'urls.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gzwrywxloihsue:0a7d0e6b5952a259c376bd8a0067725a28a27d2421a13fc6792923a7e0e994cb@ec2-50-19-114-27.compute-1.amazonaws.com:5432/demvn1fde4lip7'
+# DATABASE = 'urls.db'
+
+db = SQLAlchemy(app)
 # host = 'https://spg-redirect.herokuapp.com/'
 
+class Risperdal(db.Model):
+    __tablename__ = "Risperdal"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    fileno = db.Column(db.String(10))
+    used = db.Column(db.Integer)
 
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
+
+'''
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -20,7 +44,7 @@ def query_db(query, args=(), one=False):
     rv = cur.fetchone()
     cur.close()
     return (rv[0] if rv else None) if one else rv
-
+'''
 
 def update_count(result):
     with sqlite3.connect('urls.db') as conn:
@@ -36,18 +60,30 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/status')
+def status():
+    clients = Risperdall.query.all()
+    return render_template('status.html', data=all)
+
 @app.route('/<short_url>')
 def redirect_short_url(short_url):
     url = 'https://spg-redirect.herokuapp.com/'  # fallback if no URL is found
     try:
-        result = query_db('SELECT * FROM RISP WHERE FILENO=?', [short_url])
+        result = db.session.query(Risperdal).filter(Risperdal.fileno == short_url)
+        # result = query_db('SELECT * FROM RISP WHERE FILENO=?', [short_url])
         if result is not None and result[2] == 0:
             url = 'https://www.spglawfirm.com/risperdal-message'
+            used = int(result[2]) + 1
+            fileno = result[1]
+            db.session.add(used)
+            db.session.add(fileno)
+            '''
             with sqlite3.connect('urls.db') as conn:
                         value1 = int(result[2]) + 1
                         value2 = result[1]
                         cursor = conn.cursor()
                         cursor.execute('UPDATE RISP SET USED = ? WHERE FILENO = ?', (value1, value2))
+            '''
         elif result is None:
             message = 'We are not able to locate your case'
             return render_template('error.html', message=message)
@@ -63,4 +99,4 @@ def redirect_short_url(short_url):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
