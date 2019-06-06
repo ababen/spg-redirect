efb26fa00dffdd940b34de27ac5af038e6bffaae
Alexander Babenchuk <alexbaben@gmail.com>
2019-06-06 14:34:41 -0400

    make it better

--- app.py
+++ app.py
@@ -1,67 +1,13 @@
+from flask import Flask, request, render_template, redirect, g
+from flask_sqlalchemy import SQLAlchemy
 import os
 
-from flask import Flask, request, render_template, redirect, g
-# from sqlite3 import OperationalError
-# import sqlite3
-# from flask.ext.sqlalchemy import SQLAlchemy
-from flask_sqlalchemy import SQLAlchemy
-
-# Define database location and name
-project_dir = os.path.dirname(os.path.abspath(__file__))
-database = "sqlite:///{}".format(os.path.join(project_dir, "urls.db"))
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = database
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gzwrywxloihsue:0a7d0e6b5952a259c376bd8a0067725a28a27d2421a13fc6792923a7e0e994cb@ec2-50-19-114-27.compute-1.amazonaws.com:5432/demvn1fde4lip7'
-# DATABASE = 'urls.db'
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
-# host = 'https://spg-redirect.herokuapp.com/'
 
-class Risperdal(db.Model):
-    __tablename__ = "Risperdal"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    fileno = db.Column(db.String(10))
-    used = db.Column(db.Integer)
-
-    def __init__(self, email):
-        self.email = email
-
-    def __repr__(self):
-        return '<E-mail %r>' % self.email
-
-'''
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchone()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-'''
-
-def update_count(result):
-    new_used = int(result[2]) + 1
-    fileno = result[1]
-    client = Risperdal.query.filter_by(fileno=fileno).first()
-    client.used = new_used
-    db.session.commit()
-    return db
-    '''
-    with sqlite3.connect('urls.db') as conn:
-                value1 = int(result[2]) + 1
-                value2 = result[1]
-                cursor = conn.cursor()
-                cursor.execute('UPDATE RISP SET USED = ? WHERE FILENO = ?', (value1, value2))
-                return cursor
-    '''
+from models import Risperdal
 
 @app.route('/')
 def home():
@@ -81,14 +27,20 @@ def redirect_short_url(fileno):
     client = Risperdal.query.filter_by(fileno=fileno).first()
     if client is not None and client.used == 0:
         message = 'Not used'
-        # Update used
+        counter = client.used + 1
+        client.used = counter
+        db.session.add(client)
+        db.session.commit()
     elif client is None:
         message = 'We are not able to locate your case in our system!'
     else:
-        # Update used
-        message = 'Else'
+        counter = client.used + 1
+        client.used = counter
+        db.session.add(client)
+        db.session.commit()
+        message = 'Else: ' + chr(client.used)
     return render_template('error.html', message=message) # return redirect(url)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
---
+++ models.py
@@ -0,0 +1,15 @@
+from app import db
+
+class Risperdal(db.Model):
+    __tablename__ = "Risperdal"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    fileno = db.Column(db.String(10))
+    used = db.Column(db.Integer)
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
